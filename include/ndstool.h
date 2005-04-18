f7097d09424feee5c241d8863fb2fa8538d3a39f
Dave Murphy <wntrmute@users.sourceforge.net>
2005-04-18 10:42:13 +0000

    corrected endianness on win32

--- include/ndstool.h
+++ include/ndstool.h
@@ -13,6 +13,12 @@
 
 #ifndef __MINGW32__
 #define MAX_PATH	2048
+
+#define __LITTLE_ENDIAN 1234
+#define __BIG_ENDIAN    4321
+#define __PDP_ENDIAN    3412
+
+#define __BYTE_ORDER	__LITTLE_ENDIAN
 #endif
 
 typedef	unsigned char			u8;
