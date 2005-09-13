7fed8799df2f68e73dc5f60ee8134c08a5f60cef
Dave Murphy <wntrmute@users.sourceforge.net>
2005-09-13 03:13:36 +0000

    rework to use proper interrupt dispatcher

--- DefaultArm7/source/main.c
+++ DefaultArm7/source/main.c
@@ -1,13 +1,16 @@
 /*---------------------------------------------------------------------------------
-	$Id: main.c,v 1.8 2005-08-03 05:13:16 wntrmute Exp $
+	$Id: main.c,v 1.9 2005-09-13 03:13:36 wntrmute Exp $
 
 	Simple ARM7 stub (sends RTC, TSC, and X/Y data to the ARM 9)
 
 	$Log: not supported by cvs2svn $
+	Revision 1.8  2005/08/03 05:13:16  wntrmute
+	corrected sound code
+
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
- 
+
 #include <nds/bios.h>
 #include <nds/arm7/touch.h>
 #include <nds/arm7/clock.h>
@@ -32,101 +35,92 @@ s32 getFreeSoundChannel() {
 	return -1;
 }
 
+static u16 lastbut = 0;
+
 //---------------------------------------------------------------------------------
-void InterruptHandler(void) {
+void VblankHandler(void) {
 //---------------------------------------------------------------------------------
 	static int heartbeat = 0;
- 
-	if (IF & IRQ_VBLANK) {
-		uint16 but=0, but2=0, x=0, y=0, xpx=0, ypx=0, z1=0, z2=0, batt=0, aux=0;
-		int t1=0, t2=0;
-		uint32 temp=0;
-		uint8 ct[sizeof(IPC->curtime)];
-		u32 i;
-		
-		// Update the heartbeat
-		heartbeat++;
- 
-		// Read the X/Y buttons and the /PENIRQ line
-		but = XKEYS;
-		if (!(but & 0x40)) {
-			// Read the touch screen
 
-			touchPosition tempPos = touchReadXY();
-			
-			x = tempPos.x;
-			y = tempPos.y;
-			xpx = tempPos.px;
-			ypx = tempPos.py;
-			
-			z1 = touchRead(TSC_MEASURE_Z1);
-			z2 = touchRead(TSC_MEASURE_Z2);
+	uint16 but=0, x=0, y=0, xpx=0, ypx=0, z1=0, z2=0, batt=0, aux=0;
+	int t1=0, t2=0;
+	uint32 temp=0;
+	uint8 ct[sizeof(IPC->curtime)];
+	u32 i;
+
+
+	// Read the touch screen
+	but = REG_KEYXY;
+
+	if ( !(but & (1<<6)) && !(lastbut & (1<<6)) ) {
+
+		touchPosition tempPos = touchReadXY();
+
+
+		x = tempPos.x;
+		y = tempPos.y;
+		xpx = tempPos.px;
+		ypx = tempPos.py;
+
+		if ( x == 0 || y == 0 ) {
+			but |= (1<<6);
+		} else {
+			IPC->touchX		= x;
+			IPC->touchY		= y;
+			IPC->touchXpx	= xpx;
+			IPC->touchYpx	= ypx;
 		}
+	}
 
-		but2 = XKEYS;
-		if (!(but & 0x40) && (but2 & 0x40)) {
-			x		= 0;
-			y		= 0;
-			xpx	= 0;
-			ypx	= 0;
-			z1	= 0;
-			z2	= 0;
-			but	= but2;
-		}
-		
-		batt = touchRead(TSC_MEASURE_BATTERY);
-		aux  = touchRead(TSC_MEASURE_AUX);
+	lastbut = but;
 
-		// Read the time
-		rtcGetTime((uint8 *)ct);
-		BCDToInteger((uint8 *)&(ct[1]), 7);
- 
-		// Read the temperature
-		temp = touchReadTemperature(&t1, &t2);
- 
-		// Update the IPC struct
-		IPC->heartbeat	= heartbeat;
-		IPC->buttons		= but;
-		IPC->touchX			= x;
-		IPC->touchY			= y;
-		IPC->touchXpx		= xpx;
-		IPC->touchYpx		= ypx;
-		IPC->touchZ1		= z1;
-		IPC->touchZ2		= z2;
-		IPC->battery		= batt;
-		IPC->aux				= aux;
-
-		for(i=0; i<sizeof(ct); i++) {
-			IPC->curtime[i] = ct[i];
-		}
-
-		IPC->temperature = temp;
-		IPC->tdiode1 = t1;
-		IPC->tdiode2 = t2;
+	z1 = touchRead(TSC_MEASURE_Z1);
+	z2 = touchRead(TSC_MEASURE_Z2);
 
 
-		//sound code  :)
-		TransferSound *snd = IPC->soundData;
-		IPC->soundData = 0;
+	batt = touchRead(TSC_MEASURE_BATTERY);
+	aux  = touchRead(TSC_MEASURE_AUX);
 
-		if (0 != snd) {
+	// Read the time
+	rtcGetTime((uint8 *)ct);
+	BCDToInteger((uint8 *)&(ct[1]), 7);
+
+	// Read the temperature
+	temp = touchReadTemperature(&t1, &t2);
+
+	// Update the IPC struct
+	IPC->heartbeat	= heartbeat;
+	IPC->buttons		= but;
+	IPC->touchZ1		= z1;
+	IPC->touchZ2		= z2;
+	IPC->battery		= batt;
+	IPC->aux			= aux;
+
+	for(i=0; i<sizeof(ct); i++) {
+		IPC->curtime[i] = ct[i];
+	}
+
+	IPC->temperature = temp;
+	IPC->tdiode1 = t1;
+	IPC->tdiode2 = t2;
 
 
-			for (i=0; i<snd->count; i++) {
-				s32 chan = getFreeSoundChannel();
+	//sound code  :)
+	TransferSound *snd = IPC->soundData;
+	IPC->soundData = 0;
 
-				if (chan >= 0) {
-					startSound(snd->data[i].rate, snd->data[i].data, snd->data[i].len, chan, snd->data[i].vol, snd->data[i].pan, snd->data[i].format);
-				}
+	if (0 != snd) {
+
+		for (i=0; i<snd->count; i++) {
+			s32 chan = getFreeSoundChannel();
+
+			if (chan >= 0) {
+				startSound(snd->data[i].rate, snd->data[i].data, snd->data[i].len, chan, snd->data[i].vol, snd->data[i].pan, snd->data[i].format);
 			}
 		}
-		
-		
 	}
- 
-	// Acknowledge interrupts
-	IF = IF;
-} 
+
+}
 
 //---------------------------------------------------------------------------------
 int main(int argc, char ** argv) {
@@ -139,18 +133,13 @@ int main(int argc, char ** argv) {
 	powerON(POWER_SOUND);
 	SOUND_CR = SOUND_ENABLE | SOUND_VOL(0x7F);
 	IPC->soundData = 0;
- 
-	// Set up the interrupt handler
-	IME = 0;
-	IRQ_HANDLER = &InterruptHandler;
-	IE = IRQ_VBLANK;
-	IF = ~0;
-	DISP_SR = DISP_VBLANK_IRQ;
-	IME = 1;
-	
+
+	irqInit();
+	irqSet(IRQ_VBLANK, VblankHandler);
+	irqEnable(IRQ_VBLANK);
 
 	// Keep the ARM7 out of main RAM
 	while (1) swiWaitForVBlank();
 }
 
- 
+
